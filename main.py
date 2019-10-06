f56be3ffd1d4cc29def5475e4b485fd10faa5416
raisson souto <raissonsouto@outlook.com>
2019-10-06 20:24:41 -0300

    uma versao bem incompleta das janelas

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import sys, pygame, random
+import sys, random, pygame
+from pygame.locals import *
 
-pygame.init()
+screenSize = screenWidth, screenHeight = 1000, 600
 
-size = width, height = 1000, 600
-black = 0, 0, 0
-white = 225, 225, 225
+black = 0,0,0
+white = 255,255,255
+
+# player1 "object"
 
 player1 = {
     'width': 25,
@@ -14,84 +16,98 @@ player1 = {
     'speed': 1
 }
 
-player2 = {
-    'width': 25,
-    'height': 100,
-    'posx': 875,
-    'posy': 250,
-    'speed': 1
-}
+def telaInicial():
+    text = titulo.render('pong.io', 1, white)
+    play = fonteDefault.render('play', 1, white)
+    options = fonteDefault.render('options', 1, white)
+    credits = fonteDefault.render('credits', 1, white)
+    screen.blit(text,(400,100))
+    screen.blit(play,(300,200))
+    screen.blit(options,(300,250))
+    screen.blit(credits,(300,300))
 
-ball = {
-    'width': 20,
-    'height': 20,
-    'posx': 490,
-    'posy': 290,
-    'speedx': 1,
-    'speedy': 1
-}
+pygame.init()
 
-ball['speedx'] = random.randint(-2,3)
-ball['speedy'] = random.randint(-2,3)
+fonteMain = pygame.font.get_default_font()
+titulo = pygame.font.SysFont(fonteMain,60)
+fonteDefault = pygame.font.SysFont(fonteMain, 45)
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(screenSize)
+
+pygame.display.set_caption("")
 
 while True:
+
+    #condicoes de saida
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    events = pygame.event.get()
+    key = pygame.key.get_pressed()
 
-    #OBS: tem um bug com relacao a apertar uma tecla logo apos a outra
-
-    #movimentacao player2
-
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_w:
-            player1['posy'] -= player1['speed']
-        if event.key == pygame.K_s:
-            player1['posy'] += player1['speed']
-
-    #movimentacao player2
-
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP:
-            player2['posy'] -= player2['speed']
-        if event.key == pygame.K_DOWN:
-            player2['posy'] += player2['speed']
-
-    #movimentacao ball
-
-    ball['posx'] -= ball['speedx']
-
-    ball['posy'] -= ball['speedy']
-
-    if ball['posx'] <= 0:
-        ball['speedx'] = 0
-
-    elif ball['posx'] >= 980:
-        ball['speedx'] = 0
-
-
-    if ball['posy'] <= 0:
-        ball['speedy'] = 0
-
-    elif ball['posy'] >= 580:
-        ball['speedy'] = 0
-
+    if key[K_w] and key[K_LCTRL]:
+        sys.exit()
 
     #background
+    # testar se eu n colocar um background ele vai ficar dando o bug de repeticao
     screen.fill(black)
 
-    #player 1
-    pygame.draw.rect(screen,white,(player1['posx'],player1['posy'],player1['width'],player1['height']))
+    telaInicial()
 
-    #player 2
-    pygame.draw.rect(screen,white,(player2['posx'],player2['posy'],player2['width'],player2['height']))
-
-    #ball
-    pygame.draw.rect(screen,white,(ball['posx'],ball['posy'],ball['width'],ball['height']))
-
+    # aprender pra q serve: pygame.display.update()
     pygame.display.flip()
 
 pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''(pygame.font.init()
+
+    fonteMain = pygame.font.get_default_font()
+    titulo = pygame.font.SysFont(fonteMain,60)
+    fonteDefault = pygame.font.SysFont(fonteMain, 45)
+    text = titulo.render('pong.io', 1, white)
+    play = fonteDefault.render('play', 1, white)
+    options = fonteDefault.render('options', 1, white)
+    credits = fonteDefault.render('credits', 1, white)
+    screen.blit(text,(400,100))
+    screen.blit(play,(300,200))
+    screen.blit(options,(300,250))
+    screen.blit(credits,(300,300)))'''
+
+'''
+ #player 1 "print"
+    pygame.draw.rect(screen,white,(player1['posx'],player1['posy'],player1['width'],player1['height']))
+
+
+#background
+    # testar se eu n colocar um background ele vai ficar dando o bug de repeticao
+    screen.fill(black)
+
+    events = pygame.event.get()
+
+    #movimentacao player1
+
+    key = pygame.key.get_pressed()
+
+    if key[K_w]:
+        player1['posy'] -= player1['speed']
+
+    if key[K_s]:
+        player1['posy'] += player1['speed']
+
+    if player1['posy'] <= 0:
+        player1['posy'] = 0
+
+    elif player1['posy'] + player1['height'] >= screenHeight:
+        player1['posy'] = screenHeight - player1['height']'''
